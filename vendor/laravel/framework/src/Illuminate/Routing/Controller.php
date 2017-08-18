e2c8d21dc0a55b3bd052573f8520f4715498b8a1
Moses Esan <mosesesan@hotmail.com>
2017-08-18 10:37:11 -0400

    Revert "Removed Vendor file"
    This reverts commit 2e123df3302bf59b0edb5112fe38ee91aa896c10.

---
+++ vendor/laravel/framework/src/Illuminate/Routing/Controller.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Illuminate\Routing;
+
+use BadMethodCallException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+abstract class Controller
+{
+    /**
+     * The middleware registered on the controller.
+     *
+     * @var array
+     */
+    protected $middleware = [];
+
+    /**
+     * Register middleware on the controller.
+     *
+     * @param  array|string  $middleware
+     * @param  array   $options
+     * @return \Illuminate\Routing\ControllerMiddlewareOptions
+     */
+    public function middleware($middleware, array $options = [])
+    {
+        foreach ((array) $middleware as $middlewareName) {
+            $this->middleware[$middlewareName] = &$options;
+        }
+
+        return new ControllerMiddlewareOptions($options);
+    }
+
+    /**
+     * Get the middleware assigned to the controller.
+     *
+     * @return array
+     */
+    public function getMiddleware()
+    {
+        return $this->middleware;
+    }
+
+    /**
+     * Execute an action on the controller.
+     *
+     * @param  string  $method
+     * @param  array   $parameters
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function callAction($method, $parameters)
+    {
+        return call_user_func_array([$this, $method], $parameters);
+    }
+
+    /**
+     * Handle calls to missing methods on the controller.
+     *
+     * @param  array   $parameters
+     * @return mixed
+     *
+     * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
+     */
+    public function missingMethod($parameters = [])
+    {
+        throw new NotFoundHttpException('Controller method not found.');
+    }
+
+    /**
+     * Handle calls to missing methods on the controller.
+     *
+     * @param  string  $method
+     * @param  array   $parameters
+     * @return mixed
+     *
+     * @throws \BadMethodCallException
+     */
+    public function __call($method, $parameters)
+    {
+        throw new BadMethodCallException("Method [{$method}] does not exist.");
+    }
+}
